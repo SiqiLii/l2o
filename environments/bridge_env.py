b9aa5b82b6e56cf854ebe1f9a48f7bd7b88e116c
Siqi Li <siqili@Siqis-MacBook-Pro.local>
2024-01-29 01:26:14 -0800

    changes

--- environments/bridge_env.py
+++ environments/bridge_env.py
@@ -1,11 +1,10 @@
 import sys
-sys.path.append("/home/harry/SiQi/Code_language_2_control/l2o/environments/")
 from time import sleep
-from panda_gym_.panda_gym.envs.core_multi_robot import RobotTaskEnv
-from panda_gym_.panda_gym.pybullet import PyBullet
+from panda-gym.panda_gym.envs.core_multi_robot import RobotTaskEnv
+from panda-gym.panda_gym.pybullet import PyBullet
 import numpy as np
-from panda_gym_.bridge_task import MyTask
-from panda_gym_.panda_gym.envs.robots.panda import Panda
+from bridge_task import MyTask
+from panda-gym.panda_gym.envs.robots.panda import Panda
 class MyRobotTaskEnv(RobotTaskEnv):
     """My robot-task environment."""
 
